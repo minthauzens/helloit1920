715b3bd710c39ba4ee04ed90088e315e3418cb16
Valters <valters@echoWork>
2019-11-11 13:57:24 +0200

    finished API homework

--- Week2/getAPI.js
+++ Week2/getAPI.js
@@ -6,14 +6,72 @@
         * disable buttons if GET request hasnt gotten its response
 */
 
-const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1';
+const apiUrl = 'https://api.chucknorris.io/jokes/';
 
 $(window).on("load", () => {
-    console.log('window loaded')
+    console.log('window loaded');
+    getCategories();
+
+    //todo: only allow max 10 jokes to display
+    
 })
 
 
+function getAJoke(url) {
+    const joke = $.get(url, (response) => {
+        console.log(response);
+        $('#contentContainer').append($('<p>', { 
+            text : response.value 
+        }));
+    })
+}
 
-function getARandomDrink() {
-    
+function getARandomJoke() {
+    const url = apiUrl + "random";
+    getAJoke(url);
+}
+
+function getCategories() {
+    const url = apiUrl + "categories";
+    const categories = $.get(url, (response) => {
+        $.each(response, function (i, category) {
+            $('#categoriesSelect').append($('<option>', { 
+                value: category,
+                text : category 
+            }));
+        });
+    })
+}
+
+function getAJokeByCategory() {
+    const category = $("#categoriesSelect > option:selected").val();
+    console.log(category);
+    if (category === "") {
+        alert('please select valid category: ' + category);
+        return;
+    }
+    const url = apiUrl + `random/?category=${category}`;
+    getAJoke(url);
+}
+
+function get10JokesByQuery(){
+    const query = $("#textQuery").val();
+    console.log(query);
+    if (query === "") {
+        alert("empty query");
+        return;
+    }
+    const url = apiUrl + `search?query=${query}`;
+    $.get(url, (response) => {
+        // console.log(response);
+        $.each(response.result, function (i, joke) {
+            if (i < 10) {
+                // console.log(joke)
+                $('#contentContainer').append($('<p>', { 
+                    text : joke.value 
+                }));
+            }
+        });
+        // $("#contentContainer").append(response.value);
+    })
 }
