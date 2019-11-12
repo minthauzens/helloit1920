538c7d3085bc1dbb08ca0248b2e357b7eeb70d64
Valters <valters@echoWork>
2019-11-12 20:53:13 +0200

    week3: java lesson 2

--- Week3/java101/src/MyClass.java
+++ Week3/java101/src/MyClass.java
@@ -1,28 +1,98 @@
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class MyClass {
     // executable class because of main method
     public static void main(String[] args) {
-        MyClass test = new MyClass();
-        int c = test.sum(123, 321);
+        var array = new int[]{6, 3, 2, 7, 23, 5, 7};
+
+        int sumOfArrayElements = sum(array);
+        System.out.println("sum: " + sumOfArrayElements);
+
+        double averageOfArrayElements = average(array);
+        System.out.println("average: " + averageOfArrayElements);
+
+        int maxOfArrayElements = max(array);
+        System.out.println("max: " + maxOfArrayElements);
+
+        int minOfArrayElements = min(array);
+        System.out.println("min: " + minOfArrayElements);
+
+    }
+
+    private static int sum(int[] array) {
+        int sumElements = 0;
+        for (int el : array) {
+            sumElements += el;
+        }
+        return sumElements;
+    }
+
+    private static double average(int[] array) {
+        int sumOfArrayElements = sum(array);
+        return (double) sumOfArrayElements / array.length;
+    }
+
+    private static int min(int[] array) {
+        int minElement = array[0];
+        for (int el : array) {
+            if (el < minElement) {
+                minElement = el;
+            }
+        }
+        return minElement;
+    }
+
+    private static int max(int[] array) {
+        int maxElement = array[0];
+        for (int el : array) {
+            if (el > maxElement) {
+                maxElement = el;
+            }
+        }
+        return maxElement;
     }
 
     /**
-     *
-     * @param num1
-     * @param num2
-     * @return SUM num1 + num2
+     * @return most common element of array
      */
-    private static int sum(int num1, int num2) {
-        return num1 + num2;
+    private static Integer findMode(int[] array) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // map prepared;
+        for (int el : array) {
+            if (map.containsKey(el)) {
+                map.put(el, map.get(el) + 1);
+            } else {
+                map.put(el, 1);
+            }
+        }
+
+        // find max value:
+        int max = 0;
+        Integer mode = 0;
+        for (Integer el : map.keySet()) {
+            if (map.get(el) > max) {
+                max = map.get(el);
+                mode = el;
+            }
+        }
+
+        return mode;
     }
 
-    public static void printArray(int[] intArray) {
-        for (int i : intArray) {
-            System.out.println(i + " ");
+    public static double findMedian(int[] array) {
+        Arrays.sort(array);
+//        System.out.println(Arrays.toString(array));
+        if (array.length % 2 == 1) {
+            return array[array.length / 2 ];
+        } else {
+            return (array[array.length / 2] + array[array.length / 2 - 1]) / 2.0;
         }
     }
-
     // Sout -> System.out.println();
     // psvm -> public static void main(String[] args)
 
