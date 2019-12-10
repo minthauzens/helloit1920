49fdd633dc44c785a1bb832dd1a6a73cdc4f06f4
Valters <valters@echoWork>
2019-12-10 19:01:23 +0200

    spring init - unversioned files

---
+++ Week7/sweeter/src/test/java/lv/helloit/bootcamp/sweeter/SweeterApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.helloit.bootcamp.sweeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
